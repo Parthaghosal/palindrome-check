5e1028d25ae5ad12482c26af8e39b819830ad473
Partha Ghosal <rushpartha@gmail.com>
2019-07-01 13:58:33 +0530

    Added palindrome

---
+++ Desktop/Partho/palindrome check/index.js
@@ -0,0 +1,10 @@
+
+function isPalindrome(word) {
+  word = word.toLowerCase();
+  for (var i = 0, j = word.length-1; i < j; i++, j--) {
+    if (word[i] !== word[j]) {
+      return false;
+    }
+    return true;
+  }
+}
